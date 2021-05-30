0d71a689ce4f315a63bb3a0ac1daabe42e6755e5
Filipe Silva <filipeqs@outlook.com>
2021-05-30 16:09:14 -0400

    favicon

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const toggleSwitch = document.querySelector('input[type=checkbox]');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
@@ -7,39 +7,43 @@ const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
 // Dark or Light Images
-const imageMode = (color) => {
+function imageMode(color) {
     image1.src = `img/undraw_proud_coder_${color}.svg`;
     image2.src = `img/undraw_feeling_proud_${color}.svg`;
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-};
+}
 
-// Dark Mode Styles
-const darkMode = () => {
-    nav.style.backgroundColor = 'rgb(0 0 0 /50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 /50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
-};
-
-const lightMode = () => {
-    nav.style.backgroundColor = 'rgb(255 255 255 /50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 /50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
+const toggleDarkLightMode = (isDark) => {
+    isDark
+        ? document.documentElement.setAttribute('data-theme', 'dark')
+        : document.documentElement.setAttribute('data-theme', 'light');
+    isDark ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light');
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark
+        ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+        : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light');
 };
 
 // Switch Theme Dynamically
-const switchTheme = (e) => {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+function switchTheme(event) {
+    if (event.target.checked) {
+        toggleDarkLightMode(true);
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        toggleDarkLightMode(false);
     }
-};
+}
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
